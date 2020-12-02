66fdd9b8c969cf829db0f0f1427b17dc85abbe97
xiangxiaopenyou <xlpiOSer@163.com>
2020-12-02 14:18:02 +0800

    Initial Commit

---
+++ GoRead/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoRead
+//
+//  Created by zhangdu_imac on 2020/12/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
