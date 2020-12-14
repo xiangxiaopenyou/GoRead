60eb1d3d8285ed6affdaeec8699417f402aed54b
xiangxiaopenyou <xlpiOSer@163.com>
2020-12-14 17:45:55 +0800

    网络监测

---
+++ GoRead/Network/NetworkReachability.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkReachability.swift
+//  GoRead
+//
+//  Created by zhangdu_imac on 2020/12/14.
+//
+
+import Foundation
+import RxSwift
+import Alamofire
+
+func networkReachable() -> Observable<Bool> {
+    return NetworkReachability.shared.reachable
+}
+private class NetworkReachability {
+    static let shared = NetworkReachability()
+    let reachableSubject = ReplaySubject<Bool>.create(bufferSize: 1)
+    var reachable: Observable<Bool> {
+        return reachableSubject.asObserver()
+    }
+    init() {
+        let manager = NetworkReachabilityManager()
+        manager?.listener = { status in
+            switch status {
+            case .reachable(.ethernetOrWiFi), .reachable(.wwan):
+                self.reachableSubject.onNext(true)
+            case .notReachable, .unknown:
+                self.reachableSubject.onNext(false)
+            }
+        }
+        manager?.startListening()
+    }
+}
--- GoRead/ViewController/ViewController.swift
+++ GoRead/ViewController/ViewController.swift
@@ -39,6 +39,8 @@ class ViewController: UIViewController {
             cell.detailTextLabel?.text = String(people.age)
         }.disposed(by: disposeBag)
         
+        networkReachable()
+        
     }
 
 }
